d8deced648cedc431a8f07f36897bded5a530906
Mark Vayngrib <mark@tradle.io>
2018-02-19 09:05:20 -0500

    refactor: apigw binary support, serverless-apigw-binary is no longer needed

--- src/samplebot/configure.ts
+++ src/samplebot/configure.ts
@@ -329,7 +329,7 @@ export class Conf {
 
     const logo = await this.getLogo(conf)
     const { style } = conf
-    if (!style.logo) {
+    if (logo && !style.logo) {
       style.logo = {
         url: logo
       }
